89b1f0a3a1a438014bb95dcedc66fc2562fa9bdc
Miorey <clem.balea@gmail.com>
2019-11-03 05:58:24 +0100

    feature: - add change password command - improve doc

--- README.md
+++ README.md
@@ -6,6 +6,44 @@ On Ubuntu the following libraries have to be installed:
 ```bash
 sudo apt install -y php7.2 php7.2-xml php7.2-soap composer
 ```
+
+Install dependencies by executing:
+```bash
+composer install
+```
+
+### Start server for dev
+Setup your env vars:
+Trinitycore console requires the following vars to be setup:
+
+1. `TRINITY_USERNAME` account allowed to use trinitycore console
+3. `TRINITY_PASSWORD` account password for the account
+3. `TRINITY_ADDRESS` address of the worldserver
+
+You can also setup the server by updating **service.yaml** file like follwing
+```yaml
+    App\TrinityCore\TrinityClient:
+        arguments:
+            $username: '%env(TRINITY_USERNAME)%'
+            $password: '%env(TRINITY_PASSWORD)%'
+
+        calls:
+            - method: setServerAddress
+              arguments:
+                  - '%env(TRINITY_ADDRESS)%'
+            - methode: setServerAddress
+              arguments:
+                  - '%env(TRINITY_PORT)%'
+```
+In 90% of the cases setup the port is useless because the default value is already set
+to the default trinitycore value i.e. **7878**
+
+symfony 4 is provided with a standalone server to start it execute:
+
+```bash
+php bin/console server:start
+```
+
 ### Download and deploy the project
 
 - Git
---
+++ src/Command/AccountSetPasswordCommand.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: clem
+ * Date: 20/11/18
+ * Time: 21:58
+ */
+
+namespace App\Command;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use App\TrinityCore\TrinityClient;
+
+class AccountSetPasswordCommand extends Command
+{
+    private $trinityClient;
+
+    public function __construct(TrinityClient $trinityClient, ?string $name = null)
+    {
+        $this->trinityClient = $trinityClient;
+        parent::__construct($name);
+    }
+
+    protected function configure()
+    {
+
+        $this
+            // the name of the command (the part after "bin/console")
+            ->setName('trinity:account:set:password')
+
+            // the short description shown while running "php bin/console list"
+            ->setDescription('Change password for the given account')
+
+            // the full command description shown when running the command with
+            // the "--help" option
+            ->setHelp('Example trinity:account:set:password --account hello --password world')
+            ->addOption('account','a', InputArgument::REQUIRED, 'Principal account')
+            ->addOption('password','p', InputArgument::REQUIRED, 'New account password');
+    }
+
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     * @return int|null|void
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $command = "account set password {$input->getOption('account')} {$input->getOption('password')} {$input->getOption('password')}";
+        $this->trinityClient->executeCommand($command);
+
+    }
+
+
+}
